991792034761ab6d1ee35f5c81b747c8891ef212
kalaivani <vani212008@gmail.com>
2023-03-23 13:42:55 -0400

    js files are added

---
+++ first.js
@@ -0,0 +1,19 @@
+
+function add(){
+    var a =Number(document.getElementById("fnum").value)
+  var b=  Number(document.getElementById("snum").value)
+//   alert(a+b)
+  document.write(a+b)
+}
+function sub(){
+    var a =Number(document.getElementById("fnum").value)
+  var b=  Number(document.getElementById("snum").value)
+//   alert(a-b)
+document.write(a-b)
+}
+function mul(){
+    var a =Number(document.getElementById("fnum").value)
+  var b=  Number(document.getElementById("snum").value)
+//   alert(a*b)
+document.write(a*b)
+}
